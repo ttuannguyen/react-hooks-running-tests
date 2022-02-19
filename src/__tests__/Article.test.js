6139150692d57c4e3efa6aa462ed8cca28b1f32b
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-02-19 17:07:36 -0500

    commit tree d31ac4be16cbd4505e748e5d9777358b05fc9d2c

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -7,5 +7,8 @@ import Article from "../components/Article";
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  const element = screen.queryByText("please pass this test");
+  screen.debug(element);
+  expect(element).toBeInTheDocument();
+  //expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
